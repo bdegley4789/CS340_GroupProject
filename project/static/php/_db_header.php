b1132074bfddf9d234b4fb432b0f62317d9364f9
alessanf <alessandro.lin@hotmail.com>
2017-05-23 19:47:55 -0700

    Some changes

--- project/static/php/_db_header.php
+++ project/static/php/_db_header.php
@@ -1,7 +1,7 @@
 <?php
-	$db_host = "";
-	$db_user = "";
-	$db_pw = "";
-	$db_name = "";
+	$db_host = "tools.engr.oregonstate.edu";
+	$db_user = "cs340_alessanf";
+	$db_pw = "vhwfz4pPVJe4rssw";
+	$db_name = "cs340_alessanf";
 	$db = new mysqli($db_host, $db_user, $db_pw, $db_name);
 ?>
