497f1b9f5fcbb95d9d5386f1bc854b4c8c4891ac
bdegley4789 <egleyb@oregonstate.edu>
2017-06-02 19:08:30 -0700

    Add Course

--- project/studentEnroll.php
+++ project/studentEnroll.php
@@ -7,13 +7,18 @@ include("side.php");
 </html>
 <?php
 $mysqli = new mysqli("classmysql.engr.oregonstate.edu","cs340_alessanf","vhwfz4pPVJe4rssw","cs340_alessanf");
+if($mysqli->connect_errno) {
+  printf("Connection failed: %s\n", $mysqli->connect_error);
+}
   if ($stmt = $mysqli->prepare("insert into TakingClass(ONID,CourseID) values(?,?)")) {
 
-			$ONID = $_SESSION["ONID"];
-      $CourseID = $_REQUEST["CourseID"];
+			$ONID = $_SESSION["onidid"];
+      $CourseID = $mysqli->real_escape_string($_REQUEST["CourseID"]);
 
+      echo "$ONID";
+      echo "$CourseID";
 
-      $stmt->bind_param("is", $ONID,$CourseID);
+      $stmt->bind_param("ss", $ONID,$CourseID);
       $stmt->execute();
 
     $stmt->close();
