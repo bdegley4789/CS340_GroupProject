d7764cdb505d1ad4fee0c17a2735e12558d6c5c6
bdegley4789 <egleyb@oregonstate.edu>
2017-06-14 17:14:27 -0700

    Added Current Size for Group

--- project/add_group.php
+++ project/add_group.php
@@ -13,12 +13,17 @@ include("side.php");
 </html>
 <?php
 $mysqli = new mysqli("classmysql.engr.oregonstate.edu","cs340_alessanf","vhwfz4pPVJe4rssw","cs340_alessanf");
-echo "<table class='Group'><tr><th> Group Name  <th> Group Number <th> Size <th> Course <th> Join </tr>";
+echo "<table class='Group'><tr><th> Group Name  <th> Group Number <th> Current Size <th> Max Size <th> Course <th> Join </tr>";
 if ($result = $mysqli->query("SELECT G.Name,G.GroupID,G.size,C.subject FROM `Class`C, `Group`G WHERE C.CourseID = G.CourseID")) {
     while($obj = $result->fetch_object()){
             echo "<tr>";
             echo "<td>".htmlspecialchars($obj->Name)."</td>";
             echo "<td>".htmlspecialchars($obj->GroupID)."</td>";
+						if ($current = $mysqli->query("SELECT COUNT(GroupID) g FROM `GroupMember` WHERE GroupID = ".htmlspecialchars($obj->GroupID)."")) {
+							while($temp = $current->fetch_object()){
+								echo "<td>".htmlspecialchars($temp->g)."</td>";
+							}
+						}
             echo "<td>".htmlspecialchars($obj->size)."</td>";
 						echo "<td>".htmlspecialchars($obj->subject)."</td>";
 						echo "<td><form action='groupRecieve.php'>";
